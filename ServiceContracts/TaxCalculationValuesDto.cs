4cfbd6b870f07879913f3a5501996a6599a6758a
8414453Rs <seifoddini1365@gmail.com>
2023-04-21 12:35:05 -0700

    remove extra property on TaxCalculationValuesDto

--- ServiceContracts/TaxCalculationValuesDto.cs
+++ ServiceContracts/TaxCalculationValuesDto.cs
@@ -6,7 +6,6 @@ namespace ServiceContracts
 {
     public class TaxCalculationValuesDto
     {
-       public double TaxRate { set; get; }
         public double VAT { set; get; }
         public double Gross { set; get; }
         public double Net { set; get; }
